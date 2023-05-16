edf16887e6087c76a4cd2b11c039c5d30bfbf83b
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-05-16 11:29:51 -0300

    target_18

--- src/main/java/com/anna/controller/ZipCodeController.java
+++ src/main/java/com/anna/controller/ZipCodeController.java
@@ -1,12 +1,13 @@
 package com.anna.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -27,6 +28,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -37,6 +41,10 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ZipCodeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	// chaves de desencriptar e encriptar
+	String decKey = "NV2M5TnBxtHznZiBF85yNEP1FbnPPqvD";
+	String encKey = "lgmsTAiDqINHDQgu58gM2d3AKpPwV/tM";
+
 	public ZipCodeController() {
 		super();
 	}
@@ -45,9 +53,6 @@ public class ZipCodeController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		try {
-			// chaves de desencriptar e encriptar
-			String decKey = "NV2M5TnBxtHznZiBF85yNEP1FbnPPqvD";
-			String encKey = "lgmsTAiDqINHDQgu58gM2d3AKpPwV/tM";
 
 			// variaveis vindas no corpo da requisicao
 			String ivReceived = req.getParameter("ANNAEXEC");// esse é o iv dinâmico vindo do AnnA
@@ -66,64 +71,116 @@ public class ZipCodeController extends HttpServlet {
 			// agora e possivel desencriptar o valor do cep digitado pelo usuario
 			String zipCodeDec = decrypt(zipCode, dKey, iv);
 
-		
+			// * consulta cep
+			String url = "https://viacep.com.br/ws/" + zipCodeDec + "/json/";
 
-				String finalResponse = "[{";
-				finalResponse += "\"PropName\":\"Container001\",";
-				finalResponse += "\"PropValue\":";
-				finalResponse += "[";
-				finalResponse += "{\"";
-				finalResponse += "PropName\":\"Type\",";
-				finalResponse += "\"PropValue\":\"MESSAGE\"";
-				finalResponse += "},";
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder().uri(new URI(url)).build();
 
-				// anotherVar
-				finalResponse += "{";
-				finalResponse += "\"PropName\":\"Phrase\",";
-				finalResponse += "\"PropValue\":\"O valor de AnotherVar é : " + zipCodeDec + "\"";
-				finalResponse += "},";
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-				finalResponse += "]";
-				finalResponse += "}]";
+			if (response.statusCode() == 200) {
+				System.out.println("\n******************************************");
+				System.out.println(response.body());
+			} else {
+				System.out.println("Erro ao consultar CEP: " + response.statusCode());
+			} //
+				// extrair atributos especificos do json
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode jsonNode = mapper.readTree(response.body());
 
-				// Gerando um novo IV
-				byte[] randomBytes = new byte[8];
-				new Random().nextBytes(randomBytes);
-				final IvParameterSpec newIV = new IvParameterSpec(randomBytes);
-				String newIVEncoded = new String(Base64.getEncoder().encode(randomBytes));
+			System.out.println("\n******************************************");
 
-				// Encriptação do "IV Novo" com a chave de desencriptação e o "IV Recebido"
-				finalResponse = encrypt(finalResponse, eKey, newIV);
-				String newIVEncrip = encrypt(newIVEncoded, dKey, iv);
+			String cepPesquisado = jsonNode.get("cep").asText();
+			System.out.println("CEP: " + cepPesquisado);
 
-				// Concatenação do JSON de resposta encriptado, o "IV Recebido" e o "IV Novo
-				// Encriptado"
-				finalResponse = finalResponse + ivReceived + newIVEncrip;
+			String logradouro = jsonNode.get("logradouro").asText();
+			System.out.println("Logradouro: " + logradouro);
 
-				// Envio das informações ao AnnA
-				PrintWriter out1 = res.getWriter();
-				out1.print(finalResponse);
+			String bairro = jsonNode.get("bairro").asText();
+			System.out.println("Bairro: " + bairro);
 
-			
+			String cidade = jsonNode.get("localidade").asText();
+			System.out.println("Cidade: " + cidade);
+
+			String uf = jsonNode.get("uf").asText();
+			System.out.println("UF: " + uf);
+
+			String ddd = jsonNode.get("ddd").asText();
+			System.out.println("DDD: " + ddd);
+
+			System.out.println("******************************************\n");
+
+			String finalResponse = "Resultado para o CEP informado";
+			finalResponse += "[{";
+			finalResponse += "\"PropName\":\"Container001\",";
+			finalResponse += "\"PropValue\":";
+			finalResponse += "[";
+			finalResponse += "{\"";
+			finalResponse += "PropName\":\"Type\",";
+			finalResponse += "\"PropValue\":\"MESSAGE\"";
+			finalResponse += "},";
+
+			// cep
+			finalResponse += "{";
+			finalResponse += "\"PropName\":\"Cep\",";
+			finalResponse += "\"PropValue\":\"CEP: " + cepPesquisado + "\"";
+			finalResponse += "},";
+
+			// logradouro
+			finalResponse += "{";
+			finalResponse += "\"PropName\":\"Logradouro\",";
+			finalResponse += "\"PropValue\":\"LOGRADOURO: " + logradouro + "\"";
+			finalResponse += "},";
+
+			// bairro
+			finalResponse += "{";
+			finalResponse += "\"PropName\":\"Bairro\",";
+			finalResponse += "\"PropValue\":\"BAIRRO: " + bairro + "\"";
+			finalResponse += "},";
+
+			// cidade
+			finalResponse += "{";
+			finalResponse += "\"PropName\":\"Cidade\",";
+			finalResponse += "\"PropValue\":\"CIDADE: " + cidade + "\"";
+			finalResponse += "},";
+
+			// UF
+			finalResponse += "{";
+			finalResponse += "\"PropName\":\"UF\",";
+			finalResponse += "\"PropValue\":\"UF: " + uf + "\"";
+			finalResponse += "}";
+
+			finalResponse += "]";
+			finalResponse += "}]";
+
+			// Gerando um novo IV
+			byte[] randomBytes = new byte[8];
+			new Random().nextBytes(randomBytes);
+			final IvParameterSpec newIV = new IvParameterSpec(randomBytes);
+			String newIVEncoded = new String(Base64.getEncoder().encode(randomBytes));
+
+			// Encriptação do "IV Novo" com a chave de desencriptação e o "IV Recebido"
+			finalResponse = encrypt(finalResponse, eKey, newIV);
+			String newIVEncrip = encrypt(newIVEncoded, dKey, iv);
+
+			// Concatenação do JSON de resposta encriptado, o "IV Recebido" e o "IV Novo
+			// Encriptado"
+			finalResponse = finalResponse + ivReceived + newIVEncrip;
+
+			// Envio das informações ao AnnA
+			PrintWriter out = res.getWriter();
+			out.print(finalResponse);
 
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
 				| InvalidAlgorithmParameterException | UnsupportedEncodingException | IllegalBlockSizeException
-				| BadPaddingException ex) {
+				| BadPaddingException | URISyntaxException | InterruptedException ex) {
 			Logger.getLogger(ZipCodeController.class.getName()).log(Level.SEVERE, null, ex);
 		}
 
 	}// doPost
 
-	private String readResponse(HttpURLConnection connection) throws IOException {
-		StringBuilder response = new StringBuilder();
-		try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-		}
-		return response.toString();
-	}
+	
 
 	// metodos de encrypt e decrypt
 	public String encrypt(String message, SecretKey key, IvParameterSpec iv) throws NoSuchAlgorithmException,
