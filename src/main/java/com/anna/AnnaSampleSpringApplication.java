ed638cc2f62a2b13670d1e41d1ee8df592442672
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-05-30 12:10:26 -0300

    Agora vai!!!!!

--- src/main/java/com/anna/AnnaSampleSpringApplication.java
+++ src/main/java/com/anna/AnnaSampleSpringApplication.java
@@ -2,12 +2,18 @@ package com.anna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnnaSampleSpringApplication {
+public class AnnaSampleSpringApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnnaSampleSpringApplication.class, args);
 	}
+
+	
 
 }
